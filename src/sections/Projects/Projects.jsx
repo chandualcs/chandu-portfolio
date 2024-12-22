0dc8d4345326524935f8b2f8d8650adaff70b6d7
Laxmi Chandra Shekar Avire <chandualcs@gmail.com>
2024-12-23 01:01:39 +0530

    responsive

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,50 +4,10 @@ import stairDepth from '../../assets/clg_13.jpeg';
 import humanEmotionRecognition from '../../assets/Emotion-Detection.png';
 import BlinkIt_sales from '../../assets/blinkit_dashboard.png';
 import Pizza_sales from '../../assets/Pizzas-sales.jpg'
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
 import Card from '../Card/Card';
 import { useTheme } from '../../common/ThemeContext';
 import React, { useState } from 'react';
-
-// function Projects() {
-//   return (
-//     <section id="projects" className={styles.container}>
-//       <h1 className="sectionTitle">Projects</h1>
-//       {/* <h1 className={styles.sectionTitle}>Projects</h1> */}
-//       <div className={styles.projectsContainer}>
-//       <ProjectCard
-//           src={stairDepth}
-//           link="https://github.com/chandualcs/StairDepth-A-Novel-Staircase-detection"
-//           h3="StairDepth"
-//           p="Staircase Classification"
-//         />
-//       <ProjectCard
-//           src={humanEmotionRecognition}
-//           link="https://github.com/chandualcs/HumanEmotionRecognition"
-//           h3="Emotion Recognition"
-//           p="Human Emotion Recognition"
-//         />
-//       <ProjectCard
-//           src={BlinkIt_sales}
-//           link="https://github.com/chandualcs/Blinkit-sales-data-analysis-using-Power-BI"
-//           h3="BlinkIt sales visualization"
-//           p="PowerBI Dashboard"
-//         />
-//       <ProjectCard
-//           src={Pizza_sales}
-//           link="https://github.com/chandualcs/Pizza-sales"
-//           h3="Pizza sales analysis"
-//           p="MySQL"
-//         />
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Projects;
 
 
 const Projects = () => {
@@ -81,7 +41,7 @@ const Projects = () => {
             <Card
               image={BlinkIt_sales}
               title="BlinkIt sales Visualization"
-              description="This is a sample React project to demonstrate card components."
+              description=" Visualization and Analyzed Blinkit's sales data using Power BI to uncover key trends."
               repoLink="https://github.com/chandualcs/Blinkit-sales-data-analysis-using-Power-BI"
             />
             <Card
